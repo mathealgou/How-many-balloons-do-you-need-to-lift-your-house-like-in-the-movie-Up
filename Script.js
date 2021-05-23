29898dd209049d783fb0c1d3fab97375826222e5
mathealgou <mathealgou@gmail.com>
2021-05-23 12:52:10 -0300

    Lorem Ipsums for days!

--- Script.js
+++ Script.js
@@ -5,21 +5,26 @@ const heliumLiftInGrams = 12
 const hydrogenLiftInGrams = 13
 
 function calculate(){
-    if(document.getElementById("gasSelector").value == "Helium"){
-        calculateHelium()
+    if(document.getElementById("inputSquareMeters").value != ""){
+        if(document.getElementById("gasSelector").value == "Helium"){
+            calculateHelium()
+        }
+        if(document.getElementById("gasSelector").value == "Hydrogen"){
+            calculateHydrogen()
+        }
     }
-    if(document.getElementById("gasSelector").value == "Hydrogen"){
-        calculateHydrogen()
+    else{
+        window.alert("We need to know the size of your house fot the maths to work ;)")
     }
 }
 
 function calculateHelium(){
-    var result = weightOfHousePerSquareMeterInGrams * parseInt(document.getElementById("inputSquareMeters").value) / heliumLiftInGrams 
+    var result = weightOfHousePerSquareMeterInGrams * parseInt(document.getElementById("inputSquareMeters").value) / heliumLiftInGrams + 1
     writeResult(result)
 }
 
 function calculateHydrogen(){
-    var result = weightOfHousePerSquareMeterInGrams * parseInt(document.getElementById("inputSquareMeters").value) / hydrogenLiftInGrams 
+    var result = weightOfHousePerSquareMeterInGrams * parseInt(document.getElementById("inputSquareMeters").value) / hydrogenLiftInGrams + 1
     writeResult(result)
 }
 
